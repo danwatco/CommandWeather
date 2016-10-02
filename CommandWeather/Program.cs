cfb8093eae10d0b0e240b0f3c56f5953d8a00453
Dan Watkinson <dan.watco@gmail.com>
2016-10-02 21:14:41 +0100

    Changes order of week days dependantly, shows temps.

--- CommandWeather/Program.cs
+++ CommandWeather/Program.cs
@@ -16,20 +16,21 @@ namespace CommandWeather
     {
         public static DarkSkyService client = new DarkSkyService("d96970bf601538b3a43b1eb547d865ba");
         public static Forecast result;
+        public static double[] currentLocation = getLocation();
 
         static void Main(string[] args)
         {
-            string[] days = new string[] { "Mon".PadLeft(5), "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+            /*string[] days = new string[] { "Mon".PadLeft(10), "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
             
             foreach (string d in days)
             {
                 typeWrite(d.PadRight(7), 30, false);
                 
-            }
-            Console.WriteLine("");
+            }*/
+            displayWeekWeather();
+            //Console.WriteLine("");
             typeWrite("Hello!", 100);
-            double[] location = getLocation();
-            Task<Forecast> localRes = getWeather(location[0], location[1]);
+            Task<Forecast> localRes = getWeather(currentLocation[0], currentLocation[1]);
             typeWrite(localRes.Result.Currently.Summary, 100);
 
             Console.ReadLine();
@@ -55,7 +56,7 @@ namespace CommandWeather
             Forecast res = await client.GetWeatherDataAsync(latitude, longitude);
             result = res;
             return res;
-        }
+       } 
 
         static double[] getLocation()
         {
@@ -64,6 +65,81 @@ namespace CommandWeather
             Info res = JsonConvert.DeserializeObject<Info>(json);
             double[] ret = new double[]{ res.latitude, res.longitude };
             return ret;
+        }
+
+        static void displayWeekWeather()
+        {
+            string[] days = currentDayOrder();
+            for(int i = 0; i < days.Length; i++)
+            {
+                if(i == 0)
+                {
+                    typeWrite(days[i].PadLeft(15), 30, false);
+                }
+                else
+                {
+                    typeWrite(days[i].PadLeft(10), 30, false);
+                }
+            }
+            Console.WriteLine("");
+            Task<Forecast> localR = getWeather(currentLocation[0], currentLocation[1]);
+            string[] temps = new string[7];
+            for(int i = 0; i < temps.Length; i++)
+            {
+                int temp = Convert.ToInt32(localR.Result.Daily.Days[i].MaxTemperature);
+                temps[i] = temp.ToString();
+            }
+            List<String> tempsList = temps.ToList();
+            tempsList.Insert(0, "Temps:       ");
+            temps = tempsList.ToArray();
+            foreach(string t in temps)
+            {
+                typeWrite(t.PadRight(10), 30, false);
+            }
+            //typeWrite("Temp:", 30);
+            Console.WriteLine("");
+            /*string[] summarys = new string[7];
+            for (int i = 0; i < summarys.Length; i++)
+            {
+                string summary = localR.Result.Daily.Days[i].Summary;
+                summarys[i] = summary;
+            }
+            List<String> summarysList = summarys.ToList();
+            summarysList.Insert(0, "Summary:");
+            summarys = summarysList.ToArray();
+            foreach (string s in summarysList)
+            {
+                typeWrite(s.PadRight(10), 30, false);
+            } */ 
+
+            typeWrite("Summary: " + localR.Result.Daily.Summary, 30);
+            
+
+        }
+
+        public static string PadBoth(string source, int length)
+        {
+            int spaces = length - source.Length;
+            int padLeft = spaces / 2 + source.Length;
+            return source.PadLeft(padLeft).PadRight(length);
+
+        }
+
+        public static string[] currentDayOrder()
+        {
+            string[] dayOrder = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+            int dayInt = (int)DateTime.Today.DayOfWeek;
+            string[] res = new string[7];
+            for(int i = 0; i < res.Length; i++)
+            {
+                int dayOrderPos = dayInt + i;
+                if(dayOrderPos > 6)
+                {
+                    dayOrderPos = dayOrderPos - 7;
+                }
+                res[i] = dayOrder[dayOrderPos];
+            }
+            return res;
         }
     }
 
@@ -75,15 +151,12 @@ namespace CommandWeather
                 "zip_code":"RG12","time_zone":"Europe/London","latitude":51.4167,"longitude":-0.75,"metro_code":0}";*/
         public string country_code { get; set; }
         public string country_name { get; set; }
-        public string region_code { get; set; }
         public string region_name { get; set; }
         public string city { get; set; }
         public string zip_code { get; set; }
         public string time_zone { get; set; }
         public double latitude { get; set; }
         public double longitude { get; set; }
-        public int metro_code { get; set; }
-
     }
 
 }
